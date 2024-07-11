768e3d7f0b57ea36346af32dc03caacf2006d088
Leo Yang <leo8152257@gmail.com>
2024-07-11 11:49:44 +0800

    新增通知彈窗顯示功能，待其他網頁也插入相同片段

--- src/main/java/hibernate/util/CompositeQuery/HibernateUtil_CompositeQuery_Mem.java
+++ src/main/java/hibernate/util/CompositeQuery/HibernateUtil_CompositeQuery_Mem.java
@@ -1,87 +1,88 @@
-/*
- *  1. 萬用複合查詢-可由客戶端隨意增減任何想查詢的欄位
- *  2. 為了避免影響效能:
- *        所以動態產生萬用SQL的部份,本範例無意採用MetaData的方式,也只針對個別的Table自行視需要而個別製作之
- * */
-
 package hibernate.util.CompositeQuery;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
 import com.topseeker.member.model.MemberVO;
-
-//import hibernate.util.HibernateUtil;
-import java.util.*;
-
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery; //Hibernate 5.2 開始 取代原 org.hibernate.Criteria 介面
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.Query; //Hibernate 5 開始 取代原 org.hibernate.Query 介面
+import javax.persistence.criteria.Root;
+import javax.persistence.Query;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class HibernateUtil_CompositeQuery_Mem {
 
-	public static Predicate get_aPredicate_For_AnyDB(CriteriaBuilder builder, Root<MemberVO> root, String columnName, String value) {
+    public static Predicate get_aPredicate_For_AnyDB(CriteriaBuilder builder, Root<MemberVO> root, String columnName, String value) {
+        Predicate predicate = null;
 
-		Predicate predicate = null;
+        if ("memName".equals(columnName))
+            predicate = builder.like(root.get(columnName), "%" + value + "%");
+        else if ("memAccount".equals(columnName))
+            predicate = builder.like(root.get(columnName), "%" + value + "%");
+        else if ("memEmail".equals(columnName))
+            predicate = builder.like(root.get(columnName), "%" + value + "%");
+        else if ("memStatus".equals(columnName))
+            predicate = builder.equal(root.get(columnName), Byte.valueOf(value));
+        else if ("memDateRange".equals(columnName)) {
+            // 處理日期區間查詢
+            String[] dateRange = value.split(",");
+            if (dateRange.length == 2) {
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    Date startDate = sdf.parse(dateRange[0]);
+                    Date endDate = sdf.parse(dateRange[1]);
+                    predicate = builder.between(root.get("memBirthday"), startDate, endDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-		if ("memName".equals(columnName))
-			predicate = builder.like(root.get(columnName), "%" + value + "%");
-		else if ("memAccount".equals(columnName)) //
-			predicate = builder.like(root.get(columnName), "%" + value + "%");
-		else if ("memEmail".equals(columnName)) //
-			predicate = builder.like(root.get(columnName), "%" + value + "%");
-		else if ("memStatus".equals(columnName))
-			predicate = builder.equal(root.get(columnName), Byte.valueOf(value));
-	
+        return predicate;
+    }
 
-		return predicate;
-	}
+    @SuppressWarnings("unchecked")
+    public static List<MemberVO> getAllC(Map<String, String[]> map, Session session) {
+        Transaction tx = session.beginTransaction();
+        List<MemberVO> list = null;
+        try {
+            // 【●創建 CriteriaBuilder】
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            // 【●創建 CriteriaQuery】
+            CriteriaQuery<MemberVO> criteriaQuery = builder.createQuery(MemberVO.class);
+            // 【●創建 Root】
+            Root<MemberVO> root = criteriaQuery.from(MemberVO.class);
 
-	@SuppressWarnings("unchecked")
-	public static List<MemberVO> getAllC(Map<String, String[]> map, Session session) {
+            List<Predicate> predicateList = new ArrayList<>();
 
-//		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = session.beginTransaction();
-		List<MemberVO> list = null;
-		try {
-			// 【●創建 CriteriaBuilder】
-			CriteriaBuilder builder = session.getCriteriaBuilder();
-			// 【●創建 CriteriaQuery】
-			CriteriaQuery<MemberVO> criteriaQuery = builder.createQuery(MemberVO.class);
-			// 【●創建 Root】
-			Root<MemberVO> root = criteriaQuery.from(MemberVO.class);
+            Set<String> keys = map.keySet();
+            for (String key : keys) {
+                String value = map.get(key)[0];
+                if (value != null && value.trim().length() != 0 && !"action".equals(key)) {
+                    Predicate predicate = get_aPredicate_For_AnyDB(builder, root, key, value.trim());
+                    if (predicate != null) {
+                        predicateList.add(predicate);
+                    }
+                }
+            }
+            System.out.println("predicateList.size()=" + predicateList.size());
+            criteriaQuery.where(predicateList.toArray(new Predicate[0]));
+            criteriaQuery.orderBy(builder.asc(root.get("memNo")));
+            // 【●最後完成創建 javax.persistence.Query●】
+            Query query = session.createQuery(criteriaQuery);
+            list = query.getResultList();
 
-			List<Predicate> predicateList = new ArrayList<Predicate>();
-			
-			Set<String> keys = map.keySet();
-			int count = 0;
-			for (String key : keys) {
-				String value = map.get(key)[0];
-				if (value != null && value.trim().length() != 0 && !"action".equals(key)) {
-					count++;
-					predicateList.add(get_aPredicate_For_AnyDB(builder, root, key, value.trim()));
-//					System.out.println("有送出查詢資料的欄位數count = " + count);
-				}
-			}
-			System.out.println("predicateList.size()="+predicateList.size());
-			criteriaQuery.where(predicateList.toArray(new Predicate[predicateList.size()]));
-			criteriaQuery.orderBy(builder.asc(root.get("memNo")));
-			// 【●最後完成創建 javax.persistence.Query●】
-			Query query = session.createQuery(criteriaQuery); //javax.persistence.Query; //Hibernate 5 開始 取代原 org.hibernate.Query 介面
-			list = query.getResultList();
+            tx.commit();
+        } catch (RuntimeException ex) {
+            if (tx != null)
+                tx.rollback();
+            throw ex;
+        } finally {
+            session.close();
+        }
 
-			tx.commit();
-		} catch (RuntimeException ex) {
-			if (tx != null)
-				tx.rollback();
-			throw ex; // System.out.println(ex.getMessage());
-		} finally {
-			session.close();
-			// HibernateUtil.getSessionFactory().close();
-		}
-
-		return list;
-	}
+        return list;
+    }
 }
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,49 @@
+    $(document).ready(function(){
+
+        $(".bellIcon").on("click", function(event){
+            event.preventDefault();
+            var path = $(this).data("new-src");
+            $(this).attr("src", path);
+
+            // 加載通知數據
+            $.ajax({
+                url: '/notification/listNewNoti',
+                method: 'GET',
+                success: function(data) {
+                    $('#notificationContent').html(data);
+                    $('#notificationContainer').toggle();
+                },
+                error: function() {
+                    alert('加載通知失敗');
+                }
+            });
+        });
+
+
+        $('#notification').DataTable({
+            "lengthMenu": [10],
+            "searching": false, // 搜尋功能, 預設是開啟
+            "paging": false, // 分頁功能, 預設是開啟
+            "ordering": false, // 排序功能, 預設是開啟
+            "language": {
+                "processing": "處理中...",
+                "loadingRecords": "載入中...",
+                "lengthMenu": "顯示 _MENU_ 筆結果",
+                "zeroRecords": "沒有符合的結果",
+                "info": "顯示第 _START_ 至 _END_ 筆結果，共<font color='red'> _TOTAL_ </font>筆",
+                "infoEmpty": "顯示第 0 至 0 筆結果，共 0 筆",
+                "infoFiltered": "(從 _MAX_ 筆結果中過濾)",
+                "search": "搜尋:",
+                "paginate": {
+                    "first": "第一頁",
+                    "previous": "上一頁",
+                    "next": "下一頁",
+                    "last": "最後一頁"
+                },
+                "aria": {
+                    "sortAscending": ": 升冪排列",
+                    "sortDescending": ": 降冪排列"
+                }
+            }
+        });
+    });
