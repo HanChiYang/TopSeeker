d3ce24912e4dc297ef601a64616c1bad576ab001
K.A.H. <potw34@gmail.com>
2024-07-11 16:58:19 +0800

    修正商品更新功能；新增商品詳情、收藏頁面

--- src/main/java/com/topseeker/shop/wishlist/model/ShopWishlistService.java
+++ src/main/java/com/topseeker/shop/wishlist/model/ShopWishlistService.java
@@ -1,10 +1,11 @@
 package com.topseeker.shop.wishlist.model;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.topseeker.shop.wishlist.model.ShopWishlistVO.CompositeWishlist;
 
 @Service("ShopWishlistService")
 public class ShopWishlistService {
@@ -16,30 +17,33 @@ public class ShopWishlistService {
 	private SessionFactory sessionFactory;
 	
 	// 新增
-    public void addShopWishlist(Integer memNo, Integer prodNo) {
-        CompositeWishlist wishlistKey = new CompositeWishlist(memNo, prodNo);
-        ShopWishlistVO shopwishlistVO = new ShopWishlistVO();
-        shopwishlistVO.setCompositeWishlistKey(wishlistKey);
-        repository.save(shopwishlistVO);
+    public void addShopWishlist(ShopWishlistVO shopWishlistVO) {
+        repository.save(shopWishlistVO);
     }
 	
+	// 刪除
+	public void deletShopWishlist(Integer wishlistNo) {
+		repository.deleteById(wishlistNo);
+	}
+    
+    //透過會員號碼查詢全部收藏商品
+    public List <ShopWishlistVO> showMemWishlist(Integer memNo) {
+    	return repository.findByMemNo(memNo);
+    }
+    
+    //============架構變更，故不使用複合主鍵============
+    
     //此複合主鍵無須修改，故註解
 //	// 修改
 //	public void updateShopWishlist(ShopWishlistVO shopWishlistVO) {
 //		repository.save(shopWishlistVO);
 //	}
-	
-	// 刪除
-	public void deletShopWishlist(Integer memNo, Integer prodNo) {
-		repository.deleteByMemNoAndProdNo(memNo, prodNo);
-	}
-	
-    // 據複合主鍵查詢
-    public ShopWishlistVO getShopWishlist(Integer memNo, Integer prodNo) {
-        CompositeWishlist wishlistKey = new CompositeWishlist(memNo, prodNo);
-        return repository.findById(wishlistKey).orElse(null);
-		//有資料則回傳其值，沒有則回傳null
-    }
-
+    
+//    // 據複合主鍵查詢
+//    public ShopWishlistVO getShopWishlist(Integer memNo, Integer prodNo) {
+//        CompositeWishlist wishlistKey = new CompositeWishlist(memNo, prodNo);
+//        return repository.findById(wishlistKey).orElse(null);
+//		//有資料則回傳其值，沒有則回傳null
+//    }
 	
 }
