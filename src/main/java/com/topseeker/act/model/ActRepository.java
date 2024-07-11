e1ef597c028a6dbc57cdb6291a3f48d75c58c5d5
Leo Yang <leo8152257@gmail.com>
2024-07-11 17:19:36 +0800

    更新Notification

--- src/main/java/com/topseeker/act/model/ActRepository.java
+++ src/main/java/com/topseeker/act/model/ActRepository.java
@@ -1,13 +1,17 @@
 package com.topseeker.act.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.topseeker.follow.model.FollowVO;
 
 
+@Transactional
 public interface ActRepository extends JpaRepository<ActVO, Integer> {
 
+	@Query(value = "from ActVO where memNoA=?1 and actStatus=0 order by actStart")
+	List<ActVO> findMyOpenGroup(Integer memNoA);
 }
