db21b0a3e3e332983e2085928caf555c0aac156c
K.A.H. <potw34@gmail.com>
2024-07-12 20:36:08 +0800

    新增商城消息&新手知識model、商品收藏新增刪除功能

--- src/main/java/com/topseeker/shop/product/model/ShopProductRepository.java
+++ src/main/java/com/topseeker/shop/product/model/ShopProductRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.topseeker.shop.producttype.model.ShopProductTypeVO;
+
 public interface ShopProductRepository extends JpaRepository<ShopProductVO, Integer> {
 	
 	@Transactional
@@ -17,4 +19,15 @@ public interface ShopProductRepository extends JpaRepository<ShopProductVO, Inte
 	//透過商品類別及部分名稱模糊搜尋，並依商品上架狀態篩選，依上架日期排序
 	@Query(value = "from ShopProductVO where prod_type_no = ?1 and prod_name like?2 and prod_status = ?3 order by prod_date")
 	List<ShopProductVO> findByOthers(int prodTypeNo, String prodName, int prodStatus);
+	
+	//商城分類用:篩選已上架的商品，依上架日期排序
+	@Query(value = "SELECT * FROM shop_product WHERE prod_status = 1 ORDER BY prod_date DESC", nativeQuery = true)
+	List<ShopProductVO> getAllReleasedProd();
+	
+	//商城分類用:篩選已上架的商品，並透過商品類別篩選，依上架日期排序
+	@Query(value = "SELECT * FROM shop_product WHERE prod_type_no = ?1 AND prod_status = 1 ORDER BY prod_date DESC", nativeQuery = true)
+	List<ShopProductVO> findByProdTypeNo(int prodTypeNo);
+	
+	
+	
 }
