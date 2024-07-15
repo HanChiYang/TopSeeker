fc69d8e34e198a93ebfe956aeac3d33ae4e20669
C.Y.H <playpcfun@gmail.com>
2024-07-15 13:29:52 +0800

    新增活動留言功能

--- src/main/java/com/topseeker/act/controller/ActnoController.java
+++ src/main/java/com/topseeker/act/controller/ActnoController.java
@@ -1,7 +1,7 @@
 package com.topseeker.act.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpSession;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Digits;
@@ -14,9 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +30,9 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.*;
 import com.topseeker.act.model.ActService;
 import com.topseeker.act.model.ActVO;
+import com.topseeker.artcomment.model.ArtCommentVO;
+import com.topseeker.message.model.MessageService;
+import com.topseeker.message.model.MessageVO;
 import com.topseeker.member.model.MemberService;
 import com.topseeker.member.model.MemberVO;
 
@@ -44,6 +49,9 @@ public class ActnoController {
 	@Autowired
 	MemberService memberSvc;
 
+	@Autowired
+	MessageService messageSvc;
+	
 	/*
 	 * This method will be called on select_page.html form submission, handling POST
 	 * request It also validates the user input
@@ -75,12 +83,12 @@ public class ActnoController {
 		
 		/***************************3.查詢完成,準備轉交(Send the Success view)*****************/
 		model.addAttribute("actVO", actVO);
+		
 		model.addAttribute("getOne_For_Display", "true"); // 旗標getOne_For_Display見select_page.html的第156行 -->
 		
 //		return "back-end/emp/listOneEmp";  // 查詢完成後轉交listOneEmp.html
 		return "front-end/act/select_page"; // 查詢完成後轉交select_page.html由其第158行insert listOneEmp.html內的th:fragment="listOneEmp-div
 	}
-
 	
 	@ExceptionHandler(value = { ConstraintViolationException.class })
 	//@ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -91,8 +99,7 @@ public class ActnoController {
 	          strBuilder.append(violation.getMessage() + "<br>");
 	    }
 	    //==== 以下第92~96行是當前面第77行返回 /src/main/resources/templates/back-end/emp/select_page.html用的 ====   
-//	    model.addAttribute("empVO", new EmpVO());
-//    	EmpService empSvc = new EmpService();
+
 		List<ActVO> list = actSvc.getAll();
 		model.addAttribute("actListData", list);     // for select_page.html 第97 109行用
 		model.addAttribute("memberVO", new MemberVO());  // for select_page.html 第133行用
@@ -101,23 +108,44 @@ public class ActnoController {
 		String message = strBuilder.toString();
 	    return new ModelAndView("front-end/act/select_page", "errorMessage", "請修正以下錯誤:<br>"+message);
 	}
-	 
-//	@PostMapping("details")
-//    public String getActDetails(@RequestParam("actNo") String actNo, Model model) {
-//		System.out.println(actNo);
-//		ActVO actVO = actSvc.getOneAct(Integer.valueOf(actNo));
-//        model.addAttribute("actVO", actVO);
-//        return "back-end/act/listOneActFragment :: details";
-//    }
-	
+	//活動詳情
 	@PostMapping("details")
-    public String getActDetails(@RequestParam("actNo") String actNo, Model model) {
-//		System.out.println(actNo);
-		ActVO actVO = actSvc.getOneAct(Integer.valueOf(actNo));
+    public String getActDetails(@RequestParam("actNo") String actNo, Model model, HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+        MemberVO loggedInMember = (MemberVO) session.getAttribute("loggedInMember");
+		
+        ActVO actVO = actSvc.getOneAct(Integer.valueOf(actNo));
         model.addAttribute("actVO", actVO);
+        model.addAttribute("messageVO", new MessageVO());
         return "front-end/act/listOneAct";
     }
 	
-	
+	//新增活動留言
+		@PostMapping("/addMsg")
+	    public String addMsg(@ModelAttribute MessageVO messageVO, 
+	    		@RequestParam("actNo") Integer actNo, BindingResult result, 
+	    		Model model, HttpServletRequest request) {
+	        if (result.hasErrors()) {
+	            return "front-end/act/listOneAct";
+	        }       
+	        
+	        HttpSession session = request.getSession();
+	        MemberVO loggedInMember = (MemberVO) session.getAttribute("loggedInMember");
+
+	        if (loggedInMember != null) {
+	            messageVO.setMemberVO(loggedInMember);
+	        } else {
+	            model.addAttribute("errorMessage", "需要先登入才能留言");
+	            return "front-end/member/loginMem";
+	        }
+
+	        ActVO actVO = actSvc.getOneAct(actNo);
+	        messageVO.setActVO(actVO);
+
+	        messageSvc.addMessage(messageVO);
+//	        return "redirect:/act/getOne_For_Display?actNo=" + actNo;
+	        return "redirect:/act/listOneAct";
+	    }
 	
 }
