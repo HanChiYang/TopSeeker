ade4a0b5ff153e3952f4f756724ce2e7709bda82
Leo Yang <leo8152257@gmail.com>
2024-07-24 12:07:22 +0800

    修正開團VO驗證及頁面

--- src/main/java/com/topseeker/tourGroup/model/TourGroupVO.java
+++ src/main/java/com/topseeker/tourGroup/model/TourGroupVO.java
@@ -144,7 +144,7 @@ public class TourGroupVO implements java.io.Serializable {
 		this.groupMax = groupMax;
 	}
 	
-	@NotNull(message="請勿空白")
+//	@NotNull(message="請勿空白")
 	@Column(name = "group_status")
 	public Integer getGroupStatus() {
 		return groupStatus;
